05a357ae903f6c3d3a9cab35621bff615bc95e45
Berarma Oussama <fo_berarma@esi.dz>
2020-12-19 23:09:06 +0100

    imagesolving

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -43,8 +43,7 @@ class ProfileController extends Controller
         ]);
         if (request('image')) {
             $imagePath = request('image')->store('profile', 'public');
-            dd(public_path("storage/{$imagePath}"));
-            $image = Image::make(public_path("storage/{$imagePath}"));
+            dd(Image::make(public_path("storage/{$imagePath}")));
             $image->save();
             $imageArray = ['image' => $imagePath];
         }
