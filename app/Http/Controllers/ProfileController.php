00f03f293625fa0af2ca3ec80190120857009065
Berarma Oussama <fo_berarma@esi.dz>
2020-12-20 03:24:34 +0100

    imagesolving

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Intervention\Image\Facades\Image;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
