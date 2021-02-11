f321231d32bdfd50d6c1d1de3850fc9df93c8bb4
Berarma Oussama <fo_berarma@esi.dz>
2021-02-11 16:15:55 +0100

    vercel

---
+++ database/google_firebase.js
@@ -0,0 +1,21 @@
+/***<!-- The core Firebase JS SDK is always required and must be listed first -->***/
+src="https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js"
+
+/*** TODO: Add SDKs for Firebase products that you want to use***/
+  ///   https://firebase.google.com/docs/web/setup#available-libraries -->
+src="https://www.gstatic.com/firebasejs/8.2.1/firebase-analytics.js"
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCUwhLBU1YF4gVeL4rEE7gOcauZXvMtBLY",
+    authDomain: "laragram-37d7a.firebaseapp.com",
+    projectId: "laragram-37d7a",
+    storageBucket: "laragram-37d7a.appspot.com",
+    messagingSenderId: "811015365389",
+    appId: "1:811015365389:web:2882dc0a3d83e3198f4644",
+    measurementId: "G-YDD50KE36M"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
